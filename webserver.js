fe0251287fff95e0f1f94defd68148b331d1152b
StarKus27 <33490041+StarKus27@users.noreply.github.com>
2017-11-10 09:14:42 +0000

    Add files via upload

---
+++ webserver.js
@@ -0,0 +1,21 @@
+//Main Modules
+const express = require("express");
+const path = require("path");
+
+//Clients
+const app = express();
+
+//Routes
+app.get("/", function(request, response) {
+  response.sendFile(path.join(__dirname+"/home.html"));
+});
+
+//404
+app.get("*", function(request, response) {
+  response.sendFile(path.join(__dirname+"/404.html"));
+});
+
+//
+app.listen(process.env.PORT || 8000, function() {
+  console.log("Server running")
+});
