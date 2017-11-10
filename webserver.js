644aee896134bee3906bdf0383f67fec9892e1c7
StarKus27 <33490041+StarKus27@users.noreply.github.com>
2017-11-10 09:24:12 +0000

    Update webserver.js

--- webserver.js
+++ webserver.js
@@ -10,6 +10,10 @@ app.get("/", function(request, response) {
   response.sendFile(path.join(__dirname+"/index.html"));
 });
 
+app.get("/logo.ico", function(request, response) {
+  response.sendFile(path.join(__dirname+"/out and up.ico"));
+});
+
 //404
 app.get("*", function(request, response) {
   response.sendFile(path.join(__dirname+"/404.html"));
