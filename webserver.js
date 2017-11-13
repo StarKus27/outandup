e2aa8664579f7c2293831b2cc5780a9ce7335103
StarKus27 <33490041+StarKus27@users.noreply.github.com>
2017-11-13 10:58:02 +0000

    Update webserver.js

--- webserver.js
+++ webserver.js
@@ -1,12 +1,20 @@
 //Main Modules
 const express = require("express");
+const bodyParser = require("body-parser");
 const path = require("path");
 
 //Clients
 const app = express();
+const jsonParser = bodyParser.json()
+const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 //Routes
 app.get("/", function(request, response) {
+  response.sendFile(path.join(__dirname+"/index.html"));
+});
+
+app.post("/", urlencodedParser, function(request, response) {
+  console.log(request.body);
   response.sendFile(path.join(__dirname+"/index.html"));
 });
 
