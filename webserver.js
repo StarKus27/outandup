92e344d740cca7d05340f74be62c0d46a8eaaa56
StarKus27 <33490041+StarKus27@users.noreply.github.com>
2017-11-17 09:51:56 +0000

    Update webserver.js

--- webserver.js
+++ webserver.js
@@ -1,12 +1,20 @@
 //Main Modules
 const express = require("express");
 const bodyParser = require("body-parser");
+const nodemailer = require('nodemailer');
 const path = require("path");
 
 //Clients
 const app = express();
 const jsonParser = bodyParser.json()
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
+const transporter = nodemailer.createTransport({
+        service: 'Gmail',
+        auth: {
+                user: "outandup28@gmail.com", // Your email id
+                pass: "barackputin" // Your password
+        }
+});
 
 //Routes
 app.get("/", function(request, response) {
@@ -14,8 +22,18 @@ app.get("/", function(request, response) {
 });
 
 app.post("/", urlencodedParser, function(request, response) {
-  console.log(request.body);
-  response.sendFile(path.join(__dirname+"/index.html"));
+  let mailOptions = {
+    from: "outandup28@gmail.com", // sender address
+    to: "outandup28@gmail.com", // list of receivers
+    subject: "Out and Up: Contact Form", // Subject line
+    text: "Name: " + request.body.name + "\nEmail: " + request.body.email + "\nDescription: " + request.body.description
+  };
+  transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        console.log(error);
+    }else{
+        console.log('Message sent: ' + info.response);
+    };
 });
 
 app.get("/logo.ico", function(request, response) {
